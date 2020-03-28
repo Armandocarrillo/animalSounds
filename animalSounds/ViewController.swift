de334afc33e21f45f938cb29a24db58d91d40bc3
Armandocarrillo <ingcompuarmando@gmail.com>
2020-03-28 14:46:15 -0600

    Initial Commit

---
+++ animalSounds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animalSounds
+//
+//  Created by Armando Carrillo on 28/03/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
